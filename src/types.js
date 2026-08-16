71b1be511847cf0e186f1a38e6893235a8d97958
lehoangnamlhn2912-code <lehoangnam.lhn2912@gmail.com>
2026-08-16 09:23:20 +0700

    Delete src directory

--- src/types.js
+++
@@ -1,9 +0,0 @@
-// App Tab Constants
-export const APP_TABS = {
-  BIO_RESET: 'bio_reset',
-  FOCUS: 'focus',
-  BLIND_BREAK: 'blind_break',
-  MASCOT: 'mascot',
-  ANALYTICS: 'analytics',
-  ARCHITECTURE: 'architecture'
-};
